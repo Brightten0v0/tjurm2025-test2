67a9289f833f318e5b0b00207878da85a2ed42ea
Brightten <14312238+brightten@user.noreply.gitee.com>
2024-10-17 21:10:50 +0800

    rect cp

--- src/rect/impl.cc
+++ src/rect/impl.cc
@@ -1,5 +1,5 @@
 #include "impls.h"
-
+/*#include "utils.h"*/
 
 std::pair<cv::Rect, cv::RotatedRect> get_rect_by_contours(const cv::Mat& input) {
     /**
@@ -11,6 +11,16 @@ std::pair<cv::Rect, cv::RotatedRect> get_rect_by_contours(const cv::Mat& input) 
      * 运行测试点，你找到的矩形跟答案一样就行。
     */
     std::pair<cv::Rect, cv::RotatedRect> res;
-    // IMPLEMENT YOUR CODE HERE
+    std::vector<std::vector<cv::Point>> aim;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::Mat input_gray;
+    cv::cvtColor(input,input_gray,cv::COLOR_BGR2GRAY);
+    cv::Mat input_cp;
+    cv::threshold(input_gray,input_cp,100,255,cv::THRESH_BINARY);
+    cv::findContours(input_cp,aim,hierarchy,cv::RETR_CCOMP,cv::CHAIN_APPROX_SIMPLE);
+    /*cv::Mat aim1 = show_contours(aim, input.rows, input.cols);
+    cv::imshow("<test_find_contours> 正确的答案", aim1);*/
+    res.first=cv::boundingRect(aim[0]);
+    res.second=cv::minAreaRect(aim[0]);
     return res;
 }
